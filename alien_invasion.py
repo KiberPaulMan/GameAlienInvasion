75e6bf4957d2470c11f57b550155b15de412e2fa
Paul <twinencrypt@gmail.com>
2020-11-03 10:17:45 +0700

    Change code

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,4 +5,7 @@ import pygame
 def run_game():
     # Инициализация игры и создание объекта экрана
     pygame.init()
-    
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Запуск основного цикла игры
