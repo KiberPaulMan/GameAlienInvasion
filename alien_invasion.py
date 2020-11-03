762e4f8459d72ef81fd6bf50c45eb69960176e8f
Paul <twinencrypt@gmail.com>
2020-11-03 12:15:14 +0700

    Ship settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -11,6 +12,9 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # Создание корабля
+    ship = Ship(screen)
+
     # Запуск основного цикла игры
     while True:
         # Отслеживание событий клавиатуры и мыши
@@ -20,6 +24,7 @@ def run_game():
 
         # При каждом проходе цикла перерисовывается экран
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Отображение последнего прорисованного экрана
         pygame.display.flip()
