8a48093a301d319b4170bf124dec375794163eb8
Paul <twinencrypt@gmail.com>
2020-11-05 09:44:08 +0700

    Рефакторинг игры

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,5 @@
-import sys
 import pygame
+import game_functions as gf
 from settings import Settings
 from ship import Ship
 
@@ -17,17 +17,8 @@ def run_game():
 
     # Запуск основного цикла игры
     while True:
-        # Отслеживание событий клавиатуры и мыши
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # При каждом проходе цикла перерисовывается экран
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Отображение последнего прорисованного экрана
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
