824a0d0580c01e6fe354fcc623893e89575e9b1a
Paul <twinencrypt@gmail.com>
2020-11-05 14:40:13 +0700

    Refactoring all code

--- ship.py
+++ ship.py
@@ -3,9 +3,10 @@ import pygame
 
 class Ship:
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Инициализация корабля и задание начальной позиции"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Загрузка изображения корабля и получение прямоугольника
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,17 +16,20 @@ class Ship:
         # Каждый новый корабль появляется у нижнего края экрана
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)
 
         # Флаги перемещения корабля
         self.moving_right = False
         self.moving_left = False
 
     def update(self, step=1):
-        if self.moving_right:
-            self.rect.centerx += step
-        if self.moving_left:
-            self.rect.centerx -= step
-
+        """Обновляет позицию корабля с учетом флагов."""
+        # Обновляется атрибут center, не rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > self.screen_rect.left:
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
