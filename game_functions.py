cb0e9dc0084aaf134d45fe0d5e0152ca25c666de
Paul <twinencrypt@gmail.com>
2020-11-06 14:30:42 +0700

    the first part of the game is finish

--- game_functions.py
+++ game_functions.py
@@ -10,8 +10,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 
 def check_keyup_events(event, ship):
@@ -44,3 +43,22 @@ def update_screen(ai_settings, screen, ship, bullets):
 
     # Отображение последнего прорисованного экрана
     pygame.display.flip()
+
+
+def update_bullets(bullets):
+    """Обновляет позиции пуль и уничтожает старые пули."""
+    # Обновление позиций пуль.
+    bullets.update()
+
+    # Удаление пуль, вышедших за край экрана
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Выпускает пулю, если максимум еще не достигнут."""
+    # Создание новой пули и включение ее в группу bullets.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
--- settings.py
+++ settings.py
@@ -9,7 +9,8 @@ class Settings:
         self.ship_speed_factor = 0.5
 
         # Параметры пули
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 0.3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
