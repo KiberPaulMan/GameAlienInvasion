9249c0b56d7a6b796ba713d6f474489be888676c
Paul <twinencrypt@gmail.com>
2020-11-09 14:44:54 +0700

    Added horizontal and vertical movement

--- settings.py
+++ settings.py
@@ -6,11 +6,18 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        self.ship_speed_factor = 0.5
+        self.ship_speed_factor = 1.0
 
         # Параметры пули
         self.bullet_speed_factor = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
+
+        # Настройки пришельцев
+        self.alien_speed_factor = 1
+        # скорость снижения флота по вертикали
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
+        self.fleet_direction = 1
