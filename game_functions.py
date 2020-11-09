4bc63586a34d9a5e22c8cc98716c0df641b63fc9
Paul <twinencrypt@gmail.com>
2020-11-09 11:27:30 +0700

    Added diagonal alien placement

--- game_functions.py
+++ game_functions.py
@@ -3,6 +3,7 @@ import pygame
 from bullet import Bullet
 from alien import Alien
 
+
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Реагирует на нажатие клавиш."""
     if event.key == pygame.K_RIGHT:
@@ -67,18 +68,20 @@ def fire_bullet(ai_settings, screen, ship, bullets):
         bullets.add(new_bullet)
 
 
-def create_fleet(ai_settings, screen, aliens):
+def create_fleet(ai_settings, screen, ship, aliens):
     """Создает флот пришельцев"""
     # Создание пришельца и вычисление количества пришельцев в ряду.
     # Интервал между соседними пришельцами равен одной ширине пришельца.
     alien = Alien(ai_settings, screen)
     number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
 
     # Создание первого ряда пришельцев
-    for alien_number in range(number_aliens_x):
-        # Создание пришельца и размещение его в ряду
-        alien = Alien(ai_settings, screen)
-        create_alien(ai_settings, screen, aliens, alien_number)
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            # Создание пришельца и размещение его в ряду
+            alien = Alien(ai_settings, screen)
+            create_alien(ai_settings, screen, aliens, alien_number, row_number)
 
 
 def get_number_aliens_x(ai_settings, alien_width):
@@ -88,10 +91,19 @@ def get_number_aliens_x(ai_settings, alien_width):
     return number_aliens_x
 
 
-def create_alien(ai_settings, screen, aliens, alien_number):
+def create_alien(ai_settings, screen, aliens, alien_number, row_number):
     """Создает пришельца и размещает его в ряду."""
     alien = Alien(ai_settings, screen)
     alien_width = alien.rect.width
     alien.x = alien_width + 2 * alien_width * alien_number
     alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
     aliens.add(alien)
+
+
+def get_number_rows(ai_settings, ship_height, alien_height):
+    """Определяет количество рядов, помещающихся на экране"""
+    available_space_y = (ai_settings.screen_height - (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
+
